a2a5499f1145480eff367d8f73cb3798dd121dc8
Tyler <104091222+NerdyOreo@users.noreply.github.com>
2025-10-16 12:36:17 -0400

    Add Intune_Intranet_Rollout_Script.sanitized.ps1

---
+++ Intune_Intranet_Rollout/Intune_Intranet_Rollout_Script.sanitized.ps1
@@ -0,0 +1,16 @@
+﻿# Update these
+$Url = "<URL>
+$ShortcutName = "PLS <DOMAIN>0"
+
+$PublicDesktop = "$env:<DOMAIN>\<USERNAME>"
+$ShortcutPath = Join-Path $PublicDesktop $ShortcutName
+New-Item -ItemType Directory -Path $PublicDesktop -Force | Out-Null
+
+$lines = @(
+    "[InternetShortcut]"
+    "URL=$Url"
+    "IDList="
+    "HotKey=0"
+)
+$lines | Out-File -FilePath $ShortcutPath -Encoding ASCII -Force
+
